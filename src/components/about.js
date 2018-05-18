9e1a971d2e0934673d08aa9497e2b859c63beb2b
Dr3wbit <eightbitandrew@gmail.com>
2018-05-18 15:32:04 -0700

    Completed basic pages and Nav

---
+++ src/components/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className='center'>About us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolore placeat, itaque mollitia ut illo ipsum, ab cumque obcaecati corporis incidunt ipsam sed consectetur esse odio. Molestiae possimus laborum nulla.</p>
+    </div>
+)
